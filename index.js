2e24a5571ab88d0d15451ac3135d07d9ee9f7453
linzj3@ucweb.com <linzj3@ucweb.com>
2014-02-08 17:33:55 +0800

    [UPD]change chart js to Smoothie; [ADD]add switch

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ Log.prototype.listen = function () {
     var app = this.app;
     if (this.env === this.mode) {
 
-        app.get('/uc-Logger/log.js', function (req, res) {
+        app.get('/uc-logger/log.js', function (req, res) {
             res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
             fs.readFile(__dirname + '/lib/log/logger.js', function (err, data) {
                 if (err) throw err;
@@ -24,7 +24,7 @@ Log.prototype.listen = function () {
             });
         });
 
-        app.get('/uc-Logger/jquery.min.js', function (req, res) {
+        app.get('/uc-logger/jquery.min.js', function (req, res) {
             res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
             fs.readFile(__dirname + '/lib/cpu/js/jquery-lastest.min.js', function (err, data) {
                 if (err) throw err;
@@ -32,15 +32,15 @@ Log.prototype.listen = function () {
             });
         });
 
-        app.get('/uc-Logger/canvasjs.min.js', function (req, res) {
+        app.get('/uc-logger/smoothie.min.js', function (req, res) {
             res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
-            fs.readFile(__dirname + '/lib/cpu/js/canvasjs.min.js', function (err, data) {
+            fs.readFile(__dirname + '/lib/cpu/js/smoothie.min.js', function (err, data) {
                 if (err) throw err;
                 res.end(data);
             });
         });
 
-        app.get('/uc-Logger/cpu.js', function (req, res) {
+        app.get('/uc-logger/cpu.js', function (req, res) {
             res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
             fs.readFile(__dirname + '/lib/cpu/js/cpu.js', function (err, data) {
                 if (err) throw err;
@@ -48,13 +48,15 @@ Log.prototype.listen = function () {
             });
         });
 
-        app.get('/cpu', function(req, res){
-        	res.sendfile(__dirname + '/lib/cpu/index.html');
+        app.get('/cpu', function (req, res) {
+            res.sendfile(__dirname + '/lib/cpu/index.html');
         });
 
         var serv = http.createServer(app),
-            io = sio.listen(serv, {log: false});
+            io = sio.listen(serv),
+            viewerStatus = false;
 
+        io.set('log level', 1);
         serv.listen(app.get('port'));
 
         io.sockets.on('connection', function (socket) {
@@ -62,7 +64,7 @@ Log.prototype.listen = function () {
                 data = JSON.parse(data);
                 var infos = data['args'];
                 infos.unshift('   Page info :: ');
-                for(var i = 0, len = infos.length; i<len;i++){
+                for (var i = 0, len = infos.length; i < len; i++) {
                     infos[i] = infos[i][colorTheme[data['level']]];
                 }
                 console.log.apply(console, infos);
@@ -70,12 +72,30 @@ Log.prototype.listen = function () {
         });
 
         var monitor = io.of('/monitor').on('connection', function (socket) {
-    		socket.on('CPU monitor msg', function(data){
-            	viewer.emit('CPU mark msg', data);
+            socket.on('Monitor Ready', function () {
+                if (viewerStatus) {
+                    socket.emit('Monitor Engine Start');
+                    viewer.emit('CPU Viewer Switch Turn On');
+                }
             });
-		});
+            socket.on('CPU monitor msg', function (data) {
+                if (viewerStatus) {
+                    viewer.emit('CPU mark msg', data);
+                }
+            });
+        });
 
-		var viewer = io.of('/cpu-viewer').on('connection', function (socket) {});
+        var viewer = io.of('/cpu-viewer').on('connection', function (socket) {
+            socket.on('CPU Viewer Turn On', function (data) {
+                viewerStatus = true;
+                monitor.emit('Monitor Engine Start');
+            });
+            socket.on('CPU Viewer Turn Off', function (data) {
+                viewerStatus = false;
+                monitor.emit('Monitor Engine Cut');
+            });
+
+        });
     }
 };
 
